d9d33aa65624fe844c7b3e1eb52ac10f710427e8
young <657851574@qq.com>
2019-05-18 18:29:23 +0800

    加入编码声明

--- demo1.py
+++ demo1.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 # session
 
 import tensorflow as tf
--- demo2.py
+++ demo2.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 # variable
 
 import tensorflow as tf
@@ -6,9 +7,9 @@ state = tf.Variable(0,name='counter')
 one = tf.constant(1)
 
 new_value = tf.add(state, one)
-update = tf.assign(state, new_value)
+update = tf.assign(state, new_value)# 赋值 state = new_value
 
-init = tf.initialize_all_variables()
+init = tf.initialize_all_variables()# 初始化变量
 
 with tf.Session() as sess:
 	sess.run(init)
