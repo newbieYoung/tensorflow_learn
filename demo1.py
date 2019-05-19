d17a2df5099d77ce7d311ba8b8b2d6e6a267a5da
young <657851574@qq.com>
2019-05-19 16:17:05 +0800

    会话修改

--- demo1.py
+++ demo1.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-# 会话
+# 会话拥有并管理程序运行时的所有资源
 
 import tensorflow as tf
 
@@ -9,13 +9,20 @@ mat2 = tf.constant([[2],
 
 product = tf.matmul(mat1,mat2)
 
-# method 1
-sess = tf.Session()
-result = sess.run(product)
-print(result)
-sess.close()
+# 模式1
+# 当程序因为异常退出时，关闭会话的函数可能就不会被执行从而导致资源泄漏
+# sess = tf.Session() # 创建会话
+# print(sess.run(product)) # 执行运算
+# sess.close() # 关闭会话
 
-# method 2
-with tf.Session() as sess:
-	result2 = sess.run(product)
-	print(result2)
+# 模式2
+# 为了解决异常退出时资源释放的问题，可以使用Python的上下文管理器
+# with tf.Session() as sess:
+# 	print(sess.run(product))
+# 不需要在再调用关闭会话函数了，当上下文退出时会话关闭和资源释放也自动完成了
+
+# eval取值
+# with tf.Session() as sess:
+# 	print(product.eval())
+# with tf.Session() as sess:
+# 	print(product.eval(session=sess))
