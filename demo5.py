0e58d5809c6666b8cbd37cc82af4860d15f28812
young <657851574@qq.com>
2019-05-19 16:40:50 +0800

    简单前向传播 加入占位符

--- demo5.py
+++ demo5.py
@@ -4,11 +4,11 @@
 import tensorflow as tf
 
 # 声明 w1 w2 两个变量，并赋予正态分布随机值
-w1 = tf.Variable(tf.random_normal((2,3), stddev=1, seed=1))
-w2 = tf.Variable(tf.random_normal((3,1), stddev=1, seed=1))
+w1 = tf.Variable(tf.random_normal((2,3), stddev=1, seed=1), name="w1")
+w2 = tf.Variable(tf.random_normal((3,1), stddev=1, seed=1), name="w2")
 
-# 特征向量 1x2 矩阵
-x = tf.constant([[0.7, 0.9]])
+# 特征向量占位符（3x2 矩阵）
+x = tf.placeholder(tf.float32, shape=(3,2), name="input")
 
 #输出
 y1 = tf.matmul(x,w1)
@@ -19,5 +19,5 @@ init = tf.initialize_all_variables()# 变量初始化
 
 with tf.Session() as sess:
 	sess.run(init)
-	print(sess.run(y))
+	print(sess.run(y, feed_dict={x: [[0.7,0.9], [0.1,0.4], [0.5,0.8]]}))
 
