5d746afc55f1dc976b43ec8caa62d9d09702c6f5
young <657851574@qq.com>
2019-06-30 16:16:34 +0800

    demo4 基本程序结构 输出日志 支持 tensorboard 可视化

--- demo4.py
+++ demo4.py
@@ -3,10 +3,14 @@
 
 import tensorflow as tf
 
-v_1 = tf.constant([1,2,3,4])
-v_2 = tf.constant([2,3,4,5])
+v_1 = tf.constant([1,2,3,4], name='input1')
+v_2 = tf.constant([2,3,4,5], name='input2')
 
-v_add = tf.add(v_1,v_2)
+v_add = tf.add_n([v_1, v_2], name='add')
+
+# 生成一个写日志的 writer，并将当前的计算图写入日志
+writer = tf.summary.FileWriter('./log/demo4',tf.get_default_graph())
+writer.close()
 
 with tf.Session() as sess:
 	print(sess.run(v_add))
