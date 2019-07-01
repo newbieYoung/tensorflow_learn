d0d11adcef0263cb5d93300c2ba7ca59af03b5b9
young <657851574@qq.com>
2019-07-01 20:06:35 +0800

    简单线性回归 代码整理

--- demo0.py
+++ demo0.py
@@ -4,27 +4,29 @@
 import tensorflow as tf
 import numpy as np
 
-# create data
+# 训练数据
 x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data * 0.1 + 0.3
 
-# create tensorflow structure start #
+# 初始化线性模型参数
 Weights = tf.Variable(tf.random_uniform([1],-1.0,1.0))
 biases = tf.Variable(tf.zeros([1]))
 
+# 线性模型计算结果
 y = Weights * x_data + biases
 
+# 误差
 loss = tf.reduce_mean(tf.square(y-y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
-# create tensorflow structure end #
 
+# 梯度下降
+train = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+# 初始化变量
 init = tf.global_variables_initializer()
 
-sess = tf.Session()
-sess.run(init)
-
-for step in range(201):
-	sess.run(train)
-	if step % 20 == 0:
-		print(step,sess.run(Weights),sess.run(biases))
+with tf.Session() as sess:
+	sess.run(init)
+	for step in range(200):
+		sess.run(train)
+		if step % 20 == 0:
+			print(step,sess.run(Weights),sess.run(biases))
