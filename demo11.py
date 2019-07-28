4da69de49d016bec6b4e111b3f557e1ba460394c
young <657851574@qq.com>
2019-07-28 17:23:14 +0800

    tensorflow 持久化初步提交

---
+++ demo11.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+# tensorflow 模型持久化
+
+import tensorflow as tf
+
+# 声明两个变量并计算它们的和
+v1 = tf.Variable(tf.constant(1.0, shape=[1]), name='v1')
+v2 = tf.Variable(tf.constant(1.0, shape=[1]), name='v2')
+result = v1 + v2
+
+init_op = tf.global_variables_initializer()
+# 声明 tf.train.Saver 类用来保存模型
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(init_op)
+    print(sess.run(result))
+    # 将模型保存到 ./model/demo11.ckpt 文件
+    saver.save(sess, './model/demo11.ckpt')
