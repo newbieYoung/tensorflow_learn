568cce9f9ddeaf3d7e584fd85306a2b54845fde0
young <657851574@qq.com>
2019-07-28 16:43:41 +0800

    直接通过带有命名空间前缀的完整变量名来获取其它命名空间下的变量

--- demo10.py
+++ demo10.py
@@ -24,6 +24,7 @@ with tf.variable_scope('foo', reuse=True):
 
 # tensorflow 中 tf.variable_scope 函数是可以嵌套的，也就意味着命名空间也可以嵌套，当子命名空间不指定 reuse 参数时，这时 reuse 参数的
 # 取值会和外层命名空间的 reuse 参数保持一致。
+print('    ---    ')
 with tf.variable_scope('root'):
     # 可以通过 tf.get_variable_scope 函数来获取当前命名空间中的 reuse 参数的取值
     print(tf.get_variable_scope().reuse)
@@ -38,3 +39,30 @@ with tf.variable_scope('root'):
             print(tf.get_variable_scope().reuse)
 
     print(tf.get_variable_scope().reuse)
+
+print('    ---    ')
+# 在命名空间中创建的变量名称都会带上这个命名空间名作为前缀
+n1 = tf.get_variable('n', [1])
+print(n1.name)
+
+with tf.variable_scope('name1'):
+    with tf.variable_scope('name2'):
+        n2 = tf.get_variable('n', [1])
+        print(n2.name)
+
+    n3 = tf.get_variable('n', [1])
+    print(n3.name)
+
+# 创建一个名称为空的命名空间，并设置参数 reuse=True，然后直接通过带有命名空间前缀的完整变量名来获取其它命名空间下的变量
+with tf.variable_scope('', reuse=True):
+    n4 = tf.get_variable('name1/n', [1])
+    print(n4 == n3)
+
+    n5 = tf.get_variable('name1/name2/n', [1])
+    print(n5 == n2)
+
+    n6 = tf.get_variable('n', [1])
+    print(n6 == n1)
+
+
+
